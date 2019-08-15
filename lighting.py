fca2eb2b5562097e0f4539cb5c0818084155567e
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2019-08-14 22:17:35 -0400

    Add config file usage

--- lighting.py
+++ lighting.py
@@ -1,43 +1,33 @@
+import config
 import requests
 import sys
-from pprint import pprint
 
+# Get the command-line params
 action = sys.argv[1]
 brightness_percentage = sys.argv[2]
 
-HA_BASE_URL = ""
+# Establish constants
 HA_URL_PATH = "/api/services/homeassistant/turn_" + action
-BEARER_TOKEN = ""
 
-url = HA_BASE_URL + HA_URL_PATH
+# Build the full URL
+url = config.HA_BASE_URL + HA_URL_PATH
 
+# Create some payloads
 on_payload = '{"entity_id": "group.living_room_lights", "transition": 1.5, "brightness_pct": ' + brightness_percentage + '}'
 off_payload = '{"entity_id": "group.living_room_lights", "transition": 1.5}'
 
-headers = {
-    'Authorization': 'Bearer ' + BEARER_TOKEN,
-    'content-type': 'application/json'
-}
+# Choose which payload to use
 if action == 'on':
     payload = on_payload
 
 if action == 'off':
     payload = off_payload
 
+# Build HTTP headers
+headers = {
+    'Authorization': 'Bearer ' + config.BEARER_TOKEN,
+    'content-type': 'application/json'
+}
+
+# Send over the request to Home Assistant
 response = requests.request("POST", url, data=payload, headers=headers)
-
-
-# the filename
-#print(sys.argv[0])
-# the first after the filename: on/off
-#print(sys.argv[1])
-# the second after the filename: transition value
-#print(sys.argv[2])
-# the third after the filename: percentage brightness
-#print(sys.argv[3])
-
-# the Nth after the filename
-# print(sys.argv[N])
-
-
-
